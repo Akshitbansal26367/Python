3e95e7ee374bd952ef470806f00840479b89ac24
Akshit Bansal <akshit08082003@gmail.com>
2024-07-13 12:17:55 +0530

    Add changes

--- Chapter 04/Assignment 04/ans_03.py
+++ Chapter 04/Assignment 04/ans_03.py
@@ -4,3 +4,19 @@ chemistry = int(input("Enter marks in C : "))
 physics = int(input("Enter marks in P : "))
 english = int(input("Enter marks in E : "))
 hindi = int(input("Enter marks in H : "))
+
+percentage = (maths + chemistry + physics + english + hindi) / 5
+print("\nThe percentage is", percentage, "%")
+
+if percentage >= 90:
+    print("\nGrade A")
+elif percentage >= 80:
+    print("\nGrade B")
+elif percentage >= 60:
+    print("\nGrade C")
+elif percentage >= 50:
+    print("\nGrade D")
+elif percentage >= 40:
+    print("\nGrade E")
+else:
+    print("\nGrade F")
