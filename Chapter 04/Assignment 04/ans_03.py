9c693601c2cf7ff82224c4e6f71276b737ec42ef
Akshit Bansal <akshit08082003@gmail.com>
2024-07-13 12:07:25 +0530

    Add ans_0.py

---
+++ Chapter 04/Assignment 04/ans_03.py
@@ -0,0 +1,6 @@
+# Program to Take in marks of 5 subjects and Display the Grade
+maths = int(input("Enter marks in M : "))
+chemistry = int(input("Enter marks in C : "))
+physics = int(input("Enter marks in P : "))
+english = int(input("Enter marks in E : "))
+hindi = int(input("Enter marks in H : "))
