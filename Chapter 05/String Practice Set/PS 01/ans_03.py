60967214867563459a58cb705b05564ec6484c1a
Akshit Bansal <110843294+Akshitbansal26367@users.noreply.github.com>
2024-07-20 13:30:44 +0530

    Delete Chapter 05/String Practice Set/PS 01 directory

--- Chapter 05/String Practice Set/PS 01/ans_03.py
+++
@@ -1,9 +0,0 @@
-# Program to count occurrence of a given character in String
-s = str(input("Enter the string : "))
-s1 = str(input("Enter the character whose occurrence to be searched : "))
-count = 0
-for a in s:
-    if a == s1:
-        count += 1
-print("The occurrence of character", s1, "in the string is", count, "times")
-print("The occurrence of character", s1, "in the string is", s.count(s1), "times")
