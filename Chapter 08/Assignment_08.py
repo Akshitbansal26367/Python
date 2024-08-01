ceae019a8fbabfef1e319a859c2e910fb2de815e
Akshit Bansal <akshit08082003@gmail.com>
2024-08-01 10:44:51 +0530

    Add Assignment of Chapter 8

---
+++ Chapter 08/Assignment_08.py
@@ -0,0 +1,12 @@
+# Program to create a tuple
+tup = (2, 4, 6, 8, 1, 7, 3)
+print("Tuple is", tup)
+
+# Program to create a tuple with different data types
+tup1 = (2, "Hello", 4.6, [2, 7, 8], {'course': 'Python', 'date': '1-08-2024'})
+print("Tuple1 is", tup1)
+
+# Program to create a tuple with numbers and print one item
+tup2 = (2, 5, 7, 1, 3, 6, 8, 9)
+pos = 2
+print("Element present at position", pos, "is", tup2[pos])
