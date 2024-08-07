5523cb5e9e700893545a2b402ddc8242452c5c4d
Akshit Bansal <akshit08082003@gmail.com>
2024-08-07 10:21:36 +0530

    Change file content

--- Chapter 11/fileAppend.py
+++ Chapter 11/fileAppend.py
@@ -1,4 +1,4 @@
-# There are two ways to write in a file : write() and writelines()
+# There are two ways to append data in a file : write() and writelines()
 f = open('append.txt', 'a')
 f.write('\nHello Everyone')
 f.close()
